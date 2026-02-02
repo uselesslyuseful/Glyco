139c5973d55c1ba8076886b70cf5b1b34b6b8574
VinnieDophey <vincentpham1210@gmail.com>
2026-02-02 14:19:42 -0500

    Finished building UI for wheel picker. Just need to pass the right data to the wheel picker now (that's the bug thats showing up)

--- Glyco/Glyco/Views/ContentView.swift
+++ Glyco/Glyco/Views/ContentView.swift
@@ -168,6 +168,8 @@ struct ContentView: View {
 //    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 //}
 
+
+// MARK: - Infocard
 struct Infocard: View {
     let title: String
     let value1: String
@@ -240,26 +242,90 @@ struct Infocard: View {
     }
 }
 
+// MARK: - Wheel picker
+
 struct TimePicker: View {
-    @State private var weeks: Int = 0
-    @State private var days: Int = 0
-    @State private var hours: Int = 0
+    var style: AnyShapeStyle = .init(.bar)
+    @Binding private var weeks: Int
+    @Binding private var days: Int
+    @Binding private var hours: Int
     var body: some View{
-        PickerViewWithoutIndicator(selection:$hours){
-            ForEach(0...24, id: \.self){ value in
+        HStack(spacing: 0){
+            CustomView("hours", 0...24, $hours)
+            CustomView("days", 0...7, $days)
+            CustomView("weeks", 0...4, $weeks)
+        }
+        .offset(x: -25)
+        .background{
+            RoundedRectangle(cornerRadius: 20)
+                .fill(style)
+                .frame(height:35)
+        }
+        
+    }
+    
+    @ViewBuilder
+    private func CustomView(_ title: String, _ range: ClosedRange<Int>, _ selection: Binding<Int>) -> some View {
+        PickerViewWithoutIndicator(selection:selection){
+            ForEach(range, id: \.self){ value in
                 Text("\(value)")
+                    .frame(width: 35, alignment: .trailing)
+                    .tag(value)
             }
+        }
+        .overlay{
+            Text(title)
+                .font(.callout)
+                .frame(width: 50, alignment: .leading)
+                .lineLimit(1)
+                .offset(x: 50)
         }
     }
 }
 struct PickerViewWithoutIndicator<Content: View, Selection: Hashable>: View{
     @Binding var selection: Selection
     @ViewBuilder var content: Content
+    @State private var isHidden: Bool = false
     var body: some View{
         Picker("", selection: $selection){
+            if !isHidden {
+                RemovePickerIndicator {
+                    isHidden = true
+                }
+            }
             content
         }
         .pickerStyle(.wheel)
     }
 }
 
+// Remove background
+fileprivate
+struct RemovePickerIndicator: UIViewRepresentable {
+    var result: () -> ()
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        view.backgroundColor = .clear
+        DispatchQueue.main.async {
+            if let pickerView = view.pickerView{
+                if pickerView.subviews.count >= 2 {
+                    pickerView.subviews[1].backgroundColor = .clear
+                }
+                result()
+            }
+        }
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) { }
+}
+
+fileprivate
+extension UIView{
+    var pickerView: UIPickerView? {
+        if let view = superview as? UIPickerView {
+            return view
+        }
+        return superview?.pickerView
+    }
+}
